1bb1fadd14569c186f580f1b17000d8032fd645f
larinhax1 <larafelisbertosenai@gmail.com>
2024-05-07 11:24:52 -0300

    Update exercicio_4.py

--- entrada_saida/exercicio_4.py
+++ entrada_saida/exercicio_4.py
@@ -1,10 +1,5 @@
-# Solicitar ao usuário que insira as notas das três provas
-nota1 = float(input("Insira a nota da primeira prova: "))
-nota2 = float(input("Insira a nota da segunda prova: "))
-nota3 = float(input("Insira a nota da terceira prova: "))
-
-# Calcular a média aritmética das notas
-media = (nota1 + nota2 + nota3) / 3
-
-# Exibir o resultado da média
-print("A média das notas é:", media)
+nota1 = int(input("Insira a primeira nota: "))
+nota2 = int(input("Insira a segunda nota: "))
+nota3 = int(input("Insira a terceira nota: "))
+media = (nota1+nota2+nota3) / 2 
+print(f"A media final é: {media}")
